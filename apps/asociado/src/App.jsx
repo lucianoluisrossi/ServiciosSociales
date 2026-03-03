c440acd1d259cc23117c0ff9d6e09d99eac3aa9e
lucianoluisrossi <lucianoluisrossi@gmail.com>
2026-03-03 11:54:18 -0300

    Update App.jsx

--- apps/asociado/src/App.jsx
+++ apps/asociado/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useAuth } from "./hooks/useAuth";
-import LoginPage     from "./pages/LoginPage.jsx";
-import DashboardPage from "./pages/DashboardPage.jsx";
+import LoginPage  from "./pages/LoginPage.jsx";
+import PanelPage  from "./pages/PanelPage.jsx";
 
 function RutaProtegida({ children }) {
   const { user } = useAuth();
@@ -14,11 +14,13 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login"
+        <Route
+          path="/login"
           element={user ? <Navigate to="/" replace /> : <LoginPage />}
         />
-        <Route path="/"
-          element={<RutaProtegida><DashboardPage /></RutaProtegida>}
+        <Route
+          path="/"
+          element={<RutaProtegida><PanelPage /></RutaProtegida>}
         />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
