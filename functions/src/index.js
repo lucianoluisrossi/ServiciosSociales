8964f4dcc2bf6ee70cd395f437e6cdc7224bc82d
lucianoluisrossi <lucianoluisrossi@gmail.com>
2026-03-17 08:07:29 -0300

    Update index.js

--- functions/src/index.js
+++ functions/src/index.js
@@ -1,9 +1,27 @@
 const { initializeApp } = require("firebase-admin/app");
 initializeApp();
 
-const { onCall } = require("firebase-functions/v2/https");
+const { iniciarSesionAsociado, verificarOTPAsociado } =
+  require("./auth/sesionAsociado");
+const { activarCuenta, confirmarActivacion, buscarAsociadoParaActivar } =
+  require("./auth/activarCuenta");
+const { obtenerDatosAsociado } = require("./asociado/obtenerDatos");
+const { crearCuenta } = require("./asociado/crearCuenta");
+const { resolverSolicitud } = require("./solicitudes/resolverSolicitud");
+const { notificarNuevaSolicitud } = require("./solicitudes/notificarNuevaSolicitud");
+const { responderConfirmacionCosto } = require("./solicitudes/responderConfirmacionCosto");
+const { getSignedUrl } = require("./storage/getSignedUrl");
+const { validarFotoDNI } = require("./storage/validarFotoDNI");
 
-// Función mínima de prueba
-exports.testPing = onCall({ region: "us-east1" }, async () => {
-  return { ok: true };
-});
+exports.iniciarSesionAsociado = iniciarSesionAsociado.iniciarSesionAsociado;
+exports.verificarOTPAsociado = verificarOTPAsociado.verificarOTPAsociado;
+exports.activarCuenta = activarCuenta.activarCuenta;
+exports.confirmarActivacion = confirmarActivacion.confirmarActivacion;
+exports.buscarAsociadoParaActivar = buscarAsociadoParaActivar.buscarAsociadoParaActivar;
+exports.obtenerDatosAsociado = obtenerDatosAsociado.obtenerDatosAsociado;
+exports.crearCuenta = crearCuenta.crearCuenta;
+exports.resolverSolicitud = resolverSolicitud.resolverSolicitud;
+exports.notificarNuevaSolicitud = notificarNuevaSolicitud.notificarNuevaSolicitud;
+exports.responderConfirmacionCosto = responderConfirmacionCosto.responderConfirmacionCosto;
+exports.getSignedUrl = getSignedUrl.getSignedUrl;
+exports.validarFotoDNI = validarFotoDNI.validarFotoDNI;
